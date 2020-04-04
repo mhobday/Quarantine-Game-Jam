7e788cb505a6235369c944811e6d2cb5e8c517d2
hawk588 <gdhawk588@gmail.com>
2020-04-04 15:48:05 -0500

    comment out turrets, down movement destroys off the bottom

--- Butterfly Bullet Hell/Assets/turretAim.cs
+++ Butterfly Bullet Hell/Assets/turretAim.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
@@ -53,4 +53,4 @@ public class turretAim : MonoBehaviour
 
 
     }
-}
+}*/
