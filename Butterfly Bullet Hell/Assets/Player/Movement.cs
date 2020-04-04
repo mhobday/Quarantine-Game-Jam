7f0bd5c0399c9dfbb521137bf4929a1a6e715bee
mhobday <46661925+mhobday@users.noreply.github.com>
2020-04-04 16:31:49 -0500

    Github Sucks
    Player can shoot now too I guess

---
+++ Butterfly Bullet Hell/Assets/Player/Movement.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bullet;
+    //Used in all movement funcions to decide how far to move
+    public float baseSpeed = .05f;
+    //Delay is the amount of time before you can fie again
+    public float delay = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay--;
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(transform.position.x - baseSpeed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(transform.position.x + baseSpeed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + baseSpeed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - baseSpeed, transform.position.z);
+        }
+        if ((Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.LeftShift)) && delay < 0)
+        {
+            delay = 10f;
+            GameObject bul = Instantiate(bullet);
+            bul.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+            bul.GetComponent<Bullet>().SetMoveDirection(new Vector2(0, 1));
+
+        }
+    }
+}
