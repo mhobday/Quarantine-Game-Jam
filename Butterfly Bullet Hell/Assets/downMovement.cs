8285ada67e90200d1d3359fccf4a1a07923f0848
hawk588 <gdhawk588@gmail.com>
2020-04-04 15:43:40 -0500

    move down and turret

---
+++ Butterfly Bullet Hell/Assets/downMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class downMovement : MonoBehaviour
+{
+
+    float speed = .1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.x);
+    }
+}
