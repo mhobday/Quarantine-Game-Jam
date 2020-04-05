7a66cc878060f20c526d0f0c131da9a09af89346
mhobday <46661925+mhobday@users.noreply.github.com>
2020-04-04 19:29:29 -0500

    Bird Dies Now
    Dumb Idiot Bird Fricken Dead

---
+++ Butterfly Bullet Hell/Assets/Enemies/EnemyCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    public int health = 4;
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Friend")
+        {
+            health --;
+            if(health == 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
