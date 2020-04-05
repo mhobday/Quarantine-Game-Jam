2d3c22dbb085585ad0de3eb6abfb09c70da40ef7
mhobday <46661925+mhobday@users.noreply.github.com>
2020-04-04 22:23:59 -0500

    Caterpillar walk cycle

---
+++ Butterfly Bullet Hell/Assets/HUD/lives.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class lives : MonoBehaviour
+{
+    public GameObject player;
+    public Text stock;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //stock.text = player.hp.toString();
+    }
+}
