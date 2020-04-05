13b4921be35225def3ea20fe1fc63b21094ed2b0
mhobday <46661925+mhobday@users.noreply.github.com>
2020-04-04 21:22:08 -0500

    Cocoon Mechanic Basics

--- Butterfly Bullet Hell/Assets/Player/Health.cs
+++ Butterfly Bullet Hell/Assets/Player/Health.cs
@@ -9,6 +9,10 @@ public class Health : MonoBehaviour
     public int hp = 4;
     //Frames where you are invulnerable after being hit
     public float invFrames = 60;
+    //Cooldown is the length of time before you can cocoon
+    public float cooldown = 120f;
+    //Immune is the length of time you are immune after pressing shift
+    public float immune = 0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,19 +24,26 @@ public class Health : MonoBehaviour
     void Update()
     {
         invFrames--;
+        cooldown--;
+        immune--;
         if (invFrames > 0 && (invFrames % 5 == 0 || invFrames % 5 == 1))
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y, -20);
+            transform.position = new Vector3(transform.position.x, transform.position.y, -200);
         }
         else
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, 0);
         }
+        if (Input.GetKey(KeyCode.LeftShift) && cooldown < 0)
+        {
+            cooldown = 120f;
+            immune = 60f;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.gameObject.tag != "Friend" && invFrames < 0)
+        if (col.gameObject.tag != "Friend" && invFrames < 0 && immune < 0)
         {
             invFrames = 60;
             hp--;
